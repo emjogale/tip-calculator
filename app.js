4c804b77bb0128c0fc8003622165a0eabbdbe635
Emma <48389529+emjogale@users.noreply.github.com>
2021-09-14 21:21:22 +0100

    adding in functionality to calculate and display tip amount and total per person

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const buttons = document.querySelectorAll("button.tip");
 console.log(buttons);
 const numOfPeopleEl = document.querySelector("#num-of-people");
 console.log(numOfPeopleEl);
+const tipPerPerson = document.querySelector("#tip-per-person");
+const totalPerPerson = document.querySelector("#total");
+console.log(totalPerPerson);
+
+// TODO format answers with $ signs and tidy up code!!
+// Put in functionality to complain if number of people === 0
 
 // define the function to grab the value of the selected tip and display it
 const getPercent = (e) => {
@@ -13,11 +19,14 @@ const getPercent = (e) => {
 	const people = numOfPeopleEl.value;
 	console.log(`Divided by ${people}`);
 	let bill = Number(billEl.value);
-	let tip = Math.round((bill * 0.01 * percent) / people);
-	let total = bill / people + tip;
+	let tip = ((bill * 0.01 * percent) / people).toFixed(2);
+	let total = (Number(bill / people) + Number(tip)).toFixed(2);
+	console.log(`the total per person is ${total}`);
 	console.log(`the tip per person is ${tip}`);
 	console.log(`The total is ${total}`);
-	console.log(numOfPeopleEl.value);
+	tipPerPerson.value = tip;
+	console.log(tipPerPerson.value);
+	totalPerPerson.value = total;
 };
 
 buttons.forEach(function (button) {
