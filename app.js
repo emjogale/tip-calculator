016d90b6aae732ef1ec087e91eee46b51dbb48be
Emma <48389529+emjogale@users.noreply.github.com>
2021-09-19 20:28:18 +0100

    changing results inputs to p elements

--- app.js
+++ app.js
@@ -2,30 +2,34 @@
 const billEl = document.querySelector("#bill");
 const buttons = document.querySelectorAll("button.tip");
 const numOfPeopleEl = document.querySelector("#num-of-people");
-const tipPerPersonEl = document.querySelector("#tip-per-person");
+const tipPerPersonEl = document.querySelector("#tip-per-person span.amount");
+console.log(tipPerPersonEl);
 const totalPerPersonEl = document.querySelector("#total");
 const reset = document.querySelector(".reset");
-
-// Put in functionality to complain if number of people === 0
 
 // define the function to grab the value of the selected tip and display it
 const getPercent = (e) => {
 	e.preventDefault();
 	const percent = e.target.value;
 	const people = numOfPeopleEl.value;
-	console.log(`Divided by ${people}`);
-	let bill = Number(billEl.value);
-	let tip = ((bill * 0.01 * percent) / people).toFixed(2);
-	let total = (Number(bill / people) + Number(tip)).toFixed(2);
-	// console.log(`the total per person is ${total}`);
-	console.log(`the tip per person is ${tip}`);
-	console.log(`The total is ${total}`);
-	tipPerPersonEl.value = tip;
-	console.log(tipPerPersonEl.value);
-	totalPerPersonEl.value = total;
+	if (!people) {
+		// Put in functionality to complain if number of people === 0
+		console.log("you have to put people in !!");
+	} else {
+		console.log(`Divided by ${people}`);
+		let bill = Number(billEl.value);
+		let tip = ((bill * 0.01 * percent) / people).toFixed(2);
+		let total = (Number(bill / people) + Number(tip)).toFixed(2);
+		// console.log(`the total per person is ${total}`);
+		console.log(`the tip per person is ${tip}`);
+		console.log(`The total is ${total}`);
+		// totalPerPersonEl.textContent = tip; work out how to do this!!
+	}
 };
 
-const resetAll = () => {};
+const resetAll = () => {
+	console.log("ok so now we need to reset everything");
+};
 
 buttons.forEach(function (button) {
 	button.addEventListener("click", getPercent);
